dfcd649c53ba8c8056502c92ac15aa10825b5b80
puranchandralohar <clpuran95@gmail.com>
2023-01-20 18:22:29 +0530

    setting header

--- session/index.js
+++ session/index.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log(res)
+    res.setHeader('Content-Type', 'text/html')
+    res.write("<html>");
+    res.write("<head><title>NodeJS</title></head>");
+    res.write("<body><h1>Welcome</h1></body");
+    res.write("</html>");
+    res.end();
 })
 
  server.listen(3000);
